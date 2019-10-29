0017abc7fbe0050f85a60131b80dedb0fe656d27
Laura Archambault <laarchambault@gmail.com>
2019-10-29 22:20:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,13 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  row_index = 0
+  list = []
+  while row_index < source.length do
+    list << source[row_index]
+    row_index += 1
+  end
+  list
 end
 
 def total_gross(source)
